bcedb9bbe80b0bc2ef79af274995f79986c80e10
ducsiukap <DucPV.contact@gmail.com>
2026-04-09 12:39:47 +0700

    feat(auth): create Ãá»user model

---
+++ backend/app-be/src/main/java/com/example/app_be/model/User.java
@@ -0,0 +1,29 @@
+package com.example.app_be.model;
+
+import com.example.app_be.model.base.UuidBaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class User extends UuidBaseEntity {
+
+    @Column(name = "full_name", nullable = false, length = 150)
+    private String fullName;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+}
